7ec34b10b8e40ecb63ad14bcc54e71f2a9c455a5
MasterK <keshavagarwal12325@gmail.com>
2024-01-13 16:57:18 +0530

    first commit of animAR

---
+++ client/src/config/config.js
@@ -0,0 +1,10 @@
+const config = {
+  development: {
+    backendUrl: "http://localhost:8080/api/v1/dalle",
+  },
+  production: {
+    backendUrl: "https://animAR.netlify.app/api/v1/dalle",
+  },
+};
+
+export default config;
